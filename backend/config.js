1fb18789f91e567a072497ab7d2cc1ff87e3cc34
Rakesh <rakeshmacho2016@gmail.com>
2025-03-20 13:34:47 +0530

    backend done

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://cphrpunk:4ITwlmF0yPilGg6p@cluster0.re2rf.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0';
---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+}))
+
+app.use('/books', booksRoute);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`App is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => console.error(error));
